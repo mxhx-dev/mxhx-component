7d818d4c00a4557980ca6629b75c40ba128a6b05
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-07-27 13:00:23 -0700

    test: another inline component test with abstract

--- test/src/fixtures/TestInlineComponentClass.hx
+++ test/src/fixtures/TestInlineComponentClass.hx
@@ -4,4 +4,21 @@ class TestInlineComponentClass {
 	public function new() {}
 
 	public var classProperty:Class<Dynamic>;
+
+	public var classAbstractProperty:FromClassAbstract;
+}
+
+class Underlying {
+	public var c:Class<Dynamic>;
+
+	public function new(c:Class<Dynamic>) {
+		this.c = c;
+	}
+}
+
+abstract FromClassAbstract(Underlying) from Underlying to Underlying {
+	@:from
+	public static function fromClass(c:Class<Dynamic>):FromClassAbstract {
+		return new Underlying(c);
+	}
 }
--- test/src/mxhx/macros/MXHXComponentInlineComponentTest.hx
+++ test/src/mxhx/macros/MXHXComponentInlineComponentTest.hx
@@ -1,11 +1,12 @@
 package mxhx.macros;
 
+import fixtures.TestInlineComponentClass.Underlying;
 import fixtures.TestClass1;
 import utest.Assert;
 import utest.Test;
 
 class MXHXComponentInlineComponentTest extends Test {
-	public function testEscapedBindingInsideElement():Void {
+	public function testInlineComponentForPropertyOfTypeClass():Void {
 		var result = MXHXComponent.withMarkup('
 			<tests:TestInlineComponentClass xmlns:mx="https://ns.mxhx.dev/2024/basic" xmlns:tests="https://ns.mxhx.dev/2024/tests">
 				<tests:classProperty>
@@ -19,6 +20,25 @@ class MXHXComponentInlineComponentTest extends Test {
 		Assert.isOfType(result.classProperty, Class);
 		Assert.equals(TestClass1, Type.getSuperClass(result.classProperty));
 		var instance:Dynamic = Type.createInstance(result.classProperty, []);
+		Assert.equals(result, instance.outerDocument);
+	}
+
+	public function testInlineComponentForPropertyOfTypeAbstractFromClass():Void {
+		var result = MXHXComponent.withMarkup('
+			<tests:TestInlineComponentClass xmlns:mx="https://ns.mxhx.dev/2024/basic" xmlns:tests="https://ns.mxhx.dev/2024/tests">
+				<tests:classAbstractProperty>
+					<mx:Component>
+						<tests:TestClass1/>
+					</mx:Component>
+				</tests:classAbstractProperty>
+			</tests:TestInlineComponentClass>
+		');
+		Assert.notNull(result);
+		Assert.isOfType(result.classAbstractProperty, Underlying);
+		var underlying:Underlying = result.classAbstractProperty;
+		Assert.isOfType(underlying.c, Class);
+		Assert.equals(TestClass1, Type.getSuperClass(underlying.c));
+		var instance:Dynamic = Type.createInstance(underlying.c, []);
 		Assert.equals(result, instance.outerDocument);
 	}
 }
