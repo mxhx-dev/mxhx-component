3e3800c7c04250aa7733a248a3594d484bf1ac45
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-09-25 09:23:45 -0700

    MXHXMacroResolverTest: assert not null

--- test/src/mxhx/macros/MXHXMacroResolverTest.hx
+++ test/src/mxhx/macros/MXHXMacroResolverTest.hx
@@ -12,92 +12,110 @@ class MXHXMacroResolverTest extends Test {
 	#if !macro
 	public function testResolveAny():Void {
 		var resolved = resolveQname("Any");
+		Assert.notNull(resolved);
 		Assert.equals("Any", resolved);
 	}
 
 	public function testResolveArray():Void {
 		var resolved = resolveQname("Array");
+		Assert.notNull(resolved);
 		Assert.equals("Array", resolved);
 	}
 
 	public function testResolveBool():Void {
 		var resolved = resolveQname("Bool");
+		Assert.notNull(resolved);
 		Assert.equals("Bool", resolved);
 	}
 
 	public function testResolveStdTypesBool():Void {
 		var resolved = resolveQname("StdTypes.Bool");
+		Assert.notNull(resolved);
 		Assert.equals("Bool", resolved);
 	}
 
 	public function testResolveDynamic():Void {
 		var resolved = resolveQname("Dynamic");
+		Assert.notNull(resolved);
 		Assert.equals("Dynamic", resolved);
 	}
 
 	public function testResolveEReg():Void {
 		var resolved = resolveQname("EReg");
+		Assert.notNull(resolved);
 		Assert.equals("EReg", resolved);
 	}
 
 	public function testResolveFloat():Void {
 		var resolved = resolveQname("Float");
+		Assert.notNull(resolved);
 		Assert.equals("Float", resolved);
 	}
 
 	public function testResolveStdTypesFloat():Void {
 		var resolved = resolveQname("StdTypes.Float");
+		Assert.notNull(resolved);
 		Assert.equals("Float", resolved);
 	}
 
 	public function testResolveInt():Void {
 		var resolved = resolveQname("Int");
+		Assert.notNull(resolved);
 		Assert.equals("Int", resolved);
 	}
 
 	public function testResolveStdTypesInt():Void {
 		var resolved = resolveQname("StdTypes.Int");
+		Assert.notNull(resolved);
 		Assert.equals("Int", resolved);
 	}
 
 	public function testResolveString():Void {
 		var resolved = resolveQname("String");
+		Assert.notNull(resolved);
 		Assert.equals("String", resolved);
 	}
 
 	public function testResolveUInt():Void {
 		var resolved = resolveQname("UInt");
+		Assert.notNull(resolved);
 		Assert.equals("UInt", resolved);
 	}
 
 	public function testResolveQnameFromLocalClass():Void {
 		var resolved = resolveQname("fixtures.TestPropertiesClass");
+		Assert.notNull(resolved);
 		Assert.equals("fixtures.TestPropertiesClass", resolved);
 	}
 
 	public function testResolveArrayField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "array");
+		Assert.notNull(resolved);
 		Assert.equals("Array<String>", resolved);
 	}
 
 	public function testResolveBoolField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "boolean");
+		Assert.notNull(resolved);
 		Assert.equals("Bool", resolved);
 	}
 
 	public function testResolveClassField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "type");
+		Assert.notNull(resolved);
 		// TODO: fix the % that should be used only internally
 		Assert.equals("Class<Dynamic<%>>", resolved);
 	}
 
 	public function testResolveDateField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "date");
+		Assert.notNull(resolved);
 		Assert.equals("Date", resolved);
 	}
 
 	public function testResolveERegField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "ereg");
+		Assert.notNull(resolved);
 		Assert.equals("EReg", resolved);
 	}
 
@@ -108,36 +126,43 @@ class MXHXMacroResolverTest extends Test {
 
 	public function testResolveIntField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "integer");
+		Assert.notNull(resolved);
 		Assert.equals("Int", resolved);
 	}
 
 	public function testResolveStringField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "string");
+		Assert.notNull(resolved);
 		Assert.equals("String", resolved);
 	}
 
 	public function testResolveUIntField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "unsignedInteger");
+		Assert.notNull(resolved);
 		Assert.equals("UInt", resolved);
 	}
 
 	public function testResolveXmlField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "xml");
+		Assert.notNull(resolved);
 		Assert.equals("Xml", resolved);
 	}
 
 	public function testResolveNullField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "canBeNull");
+		Assert.notNull(resolved);
 		Assert.equals("Null<Float>", resolved);
 	}
 
 	public function testResolveStrictlyTypedField():Void {
 		var resolved = resolveQnameFieldType("fixtures.TestPropertiesClass", "strictlyTyped");
+		Assert.notNull(resolved);
 		Assert.equals("fixtures.TestPropertiesClass", resolved);
 	}
 
 	public function testResolveFieldWithTypeParameter():Void {
 		var resolved = resolveQnameFieldType("fixtures.ArrayCollection", "array");
+		Assert.notNull(resolved);
 		// TODO: fix the % that should be used only internally
 		Assert.equals("Array<%>", resolved);
 	}
